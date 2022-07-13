462de45487275778b3df5955a4a5456f94fa7993
98481 <sreeharshitha2000@gmail.com>
2022-07-13 21:02:37 +0530

    Drugs Microservice in MailOrderPharmacy

---
+++ src/main/java/com/mailorderpharmacy/drugservice/service/DrugDetailsServiceImpl.java
@@ -0,0 +1,134 @@
+package com.mailorderpharmacy.drugservice.service;
+
+import org.springframework.http.HttpStatus;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.List;
+import com.mailorderpharmacy.drugservice.entity.SuccessResponse;
+import org.springframework.http.ResponseEntity;
+import java.util.Iterator;
+import com.mailorderpharmacy.drugservice.exception.StockNotFoundException;
+import com.mailorderpharmacy.drugservice.entity.DrugLocationDetails;
+import com.mailorderpharmacy.drugservice.entity.Stock;
+import com.mailorderpharmacy.drugservice.exception.InvalidTokenException;
+import com.mailorderpharmacy.drugservice.exception.DrugNotFoundException;
+import com.mailorderpharmacy.drugservice.entity.TokenValid;
+import com.mailorderpharmacy.drugservice.entity.DrugDetails;
+import com.mailorderpharmacy.drugservice.restclients.AuthFeign;
+
+import com.mailorderpharmacy.drugservice.dao.DrugLocationRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.mailorderpharmacy.drugservice.dao.DrugDetailsRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DrugDetailsServiceImpl implements DrugDetailsService
+{
+
+    @Autowired
+    private DrugDetailsRepository drugRepo;
+    @Autowired
+    private DrugLocationRepository locationRepo;
+    @Autowired
+    private AuthFeign authFeign;
+    
+	private static final Logger log = LoggerFactory.getLogger(DrugDetailsServiceImpl.class);
+
+    @Override
+    public DrugDetails getDrugById(final String id, final String token) throws InvalidTokenException, DrugNotFoundException {
+        DrugDetails drugDetails = null;
+        if (((TokenValid)this.authFeign.getValidity(token).getBody()).isValid()) {
+            drugDetails = this.drugRepo.findById(id).orElseThrow(() -> new DrugNotFoundException("Drug Not Found"));
+            return drugDetails;
+        }
+        throw new InvalidTokenException("Invalid Credentials");
+    }
+    
+    @Override
+    public DrugDetails getDrugByName(final String name, final String token) throws InvalidTokenException, DrugNotFoundException {
+    	log.info("start--serviceimpl--getDrugByName");
+    	if (((TokenValid)this.authFeign.getValidity(token).getBody()).isValid()) {
+            try {
+                return this.drugRepo.findBydrugName(name).get();
+            }
+            catch (Exception e) {
+                throw new DrugNotFoundException("Drug Not Found");
+            }
+        }
+    	else
+    		throw new InvalidTokenException("Invalid Credentials");
+    }
+    
+    @Override
+    public Stock getDispatchableDrugStock(final String id, final String location, final String token) throws InvalidTokenException, StockNotFoundException, DrugNotFoundException {
+        if (!((TokenValid)this.authFeign.getValidity(token).getBody()).isValid()) {
+            throw new InvalidTokenException("Invalid Credentials");
+        }
+        DrugDetails details = null;
+        try {
+            details = this.drugRepo.findById(id).get();
+        }
+        catch (Exception e) {
+            throw new DrugNotFoundException("Drug Not Found");
+        }
+        Stock stock = null;
+        for (final DrugLocationDetails dld : details.getDruglocationQuantities()) {
+            if (dld.getLocation().equals(location)) {
+                stock = new Stock(id, details.getDrugName(), details.getExpiryDate(), dld.getQuantity());
+            }
+        }
+        if (stock == null) {
+            throw new StockNotFoundException("Stock Unavailable at your location");
+        }
+        return stock;
+    }
+    
+    @Override
+    public ResponseEntity<SuccessResponse> updateQuantity(final String drugName, final String location, final int quantity, final String token) throws InvalidTokenException, DrugNotFoundException, StockNotFoundException {
+       
+    
+        log.info("start--serviceimpl-- updateQuantity");
+		if (authFeign.getValidity(token).getBody().isValid()) {
+			DrugDetails details = new DrugDetails();
+			try {
+				details = this.drugRepo.findBydrugName(drugName).get();
+			} catch (Exception e) {
+
+				throw new DrugNotFoundException("Drug Not Found");
+			}
+			List<DrugLocationDetails> dummy = details.getDruglocationQuantities().stream()
+					.filter(x -> 
+					x.getLocation().equalsIgnoreCase(location))
+					.collect(Collectors.toList());
+
+			if (dummy.isEmpty()) {
+				throw new StockNotFoundException("Stock Unavailable at your location");
+			}
+
+			else if (dummy.get(0).getQuantity() >= quantity && quantity >0) {
+
+				DrugLocationDetails allDetails = locationRepo.findByserialId(dummy.get(0).getSerialId()).get(0);
+				int val = allDetails.getQuantity() - quantity;
+				allDetails.setQuantity(val);
+				this.locationRepo.save(allDetails);
+				log.info("End--serviceimpl-- updateQuantity");
+				return (ResponseEntity<SuccessResponse>)new ResponseEntity((Object)new SuccessResponse("Refill Done Successfully"), HttpStatus.OK);
+			} else
+				throw new StockNotFoundException("Stock Unavailable at your location");
+		}
+		throw new InvalidTokenException("Invalid Token Received");
+		
+		
+		
+    
+    }
+    
+    @Override
+    public List<DrugDetails> getAllDrugs() {
+        final List<DrugDetails> drugList = (List<DrugDetails>)this.drugRepo.findAll();
+        return drugList;
+    }
+}
---
+++ target/classes/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO DRUG_DETAILS VALUES ('D1', 'Paracetmol', parsedatetime('22-11-2024', 'dd-MM-yyyy'), parsedatetime('22-02-202', 'dd-MM-yyyy'), 'Lalitha');
+INSERT INTO DRUG_DETAILS VALUES ('D2', 'Citrazine', parsedatetime('22-11-2024', 'dd-MM-yyyy'), parsedatetime('22-02-2022', 'dd-MM-yyyy'), 'Phani Sandeep');
+INSERT INTO DRUG_DETAILS VALUES ('D3', 'Aspirin', parsedatetime('22-11-2024', 'dd-MM-yyyy'), parsedatetime('22-02-2022', 'dd-MM-yyyy'), 'Subharthi Pharma');
+INSERT INTO DRUG_DETAILS VALUES ('D4', 'Dolo', parsedatetime('22-11-2024', 'dd-MM-yyyy'), parsedatetime('22-02-2022', 'dd-MM-yyyy'), 'A.K.Pharma');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('1', 'Chennai', 300, 'D1');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('2', 'Bangalore', 400, 'D1');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('3', 'Pune', 300, 'D1');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('4', 'Hyderabad', 500, 'D1');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('5', 'Chennai', 300, 'D2');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('6', 'Bangalore', 200, 'D2');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('7', 'Pune', 300, 'D2');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('8', 'Hyderabad', 300, 'D2');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('9', 'Chennai', 300, 'D3');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('10', 'Bangalore', 500, 'D3');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('11', 'Hyderabad', 400, 'D4');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('12', 'Chennai', 300, 'D4');
+INSERT INTO DRUG_LOCATION_DETAILS VALUES('13', 'Bangalore', 500, 'D4');
